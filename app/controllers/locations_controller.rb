ed46975661d9728f803c09ce55a45cf8f773c246
theouternet <jrougeau@gmail.com>
2020-06-10 20:06:52 -0500

    locationscontroller index json

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,14 @@ class LocationsController < ApplicationController
 
     def index
         @locations = Location.all
+
+        respond_to do |format|
+          format.html { render :index }
+          format.json { render json: @locations }
+        end 
+
     end
+    
   
     def show
         @location = Location.find(params[:id])
